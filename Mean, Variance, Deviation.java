9fa154ccc57ed2a85e84ffe9cee82fcbc24d7f14
HargunBhalla <38545675+HargunBhalla@users.noreply.github.com>
2018-12-24 16:28:45 -0500

    Update Mean, Variance, Deviation.java

--- Mean, Variance, Deviation.java
+++ Mean, Variance, Deviation.java
@@ -13,16 +13,16 @@ for (double y = 1; y <= counter; y++){
 }
 double mean = sum / counter;
 System.out.println("The mean is: " +mean);
-//Output for the mean!
+//Output for the mean
   
       double variance = sum -((Math.pow (sum, 2)/counter))/counter-1;
-
+//Variance formula
        double deviation = Math.pow (variance, 0.5); 
-
+//Standard Deviation formula
        System.out.println("The variance is:  "+variance);
+//Output for the variance
        System.out.println("The standard deviation is: "+deviation);
-
+//Output for the standard deviation
     }
 
-    }
- 
+}
